6093d605e678ff3debbfe9668bf74b805f7237a8
Eevee89 <149712903+Eevee89@users.noreply.github.com>
2024-10-03 23:04:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function getRandomPokemon() {
+    let index = Math.floor(Math.random() * 996);
+    return pokemons[index];
+}
+
+let poke = "";
+let found = 0;
+let errors = 0;
+let score = 0;
+
+$(document).ready(function () {
+    poke = getRandomPokemon().toUpperCase();
+    for(i=0; i<poke.length; i++) {
+        $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
+    } 
+    $(".character").css("width", 90/poke.length + "%");
+
+    $("#replay").click(() => {
+        $("#row").html("");
+        poke = getRandomPokemon().toUpperCase();
+        for(i=0; i<poke.length; i++) {
+            $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
+        } 
+        $(".character").css("width", 90/poke.length + "%");
+    });
+});
+
+
+$(document).keydown(function(e) {
+    let code = e.keyCode;
+    if (code >= 65 && code <= 90) {
+        for(i=0; i<poke.length; i++) {
+            if (poke[i] == String.fromCharCode(code)) {
+                $("#row").children()[i].innerHTML = "<p>"+ poke[i] +"</p>";
+                found += 1;
+            }
+        }
+        if (poke.search(String.fromCharCode(code)) === -1) {
+            errors += 1;
+            $("#errors").text("Erreurs : "+errors+"/10");
+        }
+        $("#letters").text($("#letters").text()+" "+String.fromCharCode(code));
+    }
+    if (found === poke.length) {
+        $(".character").css("background-color", "#0F0");
+        errors = 0;
+        found = 0;
+        score += 1;
+        $("#score").text("Score : "+score);
+    }
+    if (errors === 10) {
+        $(".character").css("background-color", "#F00");
+        for(i=0; i<poke.length; i++) {
+            $("#row").children()[i].innerHTML = "<p>"+ poke[i] +"</p>";
+        }
+        errors = 0;
+        found = 0;
+    }
+});
