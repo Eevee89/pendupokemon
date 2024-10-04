d22b89f6ded1e0c721f22bc6f95a25fa8f0486f1
Eevee89 <149712903+Eevee89@users.noreply.github.com>
2024-10-04 13:04:57 +0200

    Add files via upload

--- php/mobile.php
+++ php/mobile.php
@@ -4,13 +4,13 @@
         <img id="pokemon_logo" src="pokemon_logo.png">
     </div>
     <div id="row"></div>
-    <div id="bottom">
-        <p id="replay">Rejouer</p>
-        <p id="score">Score : 0</p>
-    </div>
     <div id="guess">
         <p id="errors">Erreurs : 0/10</p>
         <p id="letters">Lettres :</p>
+    </div>
+    <div id="bottom">
+        <p id="replay">Rejouer</p>
+        <p id="score">Score : 0</p>
     </div>
     <div id="tiles">
         <?php for($i=0; $i<6; $i++): ?>
