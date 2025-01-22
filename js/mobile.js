d27843a22b72fdde3967dc572d49a2d8b51ce562
joris <jorismartin3@gmail.com>
2025-01-22 16:31:04 +0100

    Convert client-side into server-side (mobile)

--- js/mobile.js
+++ js/mobile.js
@@ -1,22 +1,3 @@
-function getRandomPokemon(gens) {
-    let filtered = [];
-    pokemons.forEach((poke) => {
-        if (gens.includes(poke["Generation"])) {
-            filtered.push(poke);
-        }
-    });
-    let index = Math.floor(Math.random() * filtered.length);
-    return filtered[index];
-}
-
-let poke = "";
-let nom = "";
-let pokedex = -1;
-let types = [];
-let found = 0;
-let errors = 0;
-let score = 0;
-
 $(document).ready(function () {
     $("#hint").removeClass("hint");
     $("#type1").attr("src", "");
@@ -29,18 +10,17 @@ $(document).ready(function () {
             gens.push(i);
         }
     }
-    poke = getRandomPokemon(gens);
-    pokedex = poke["Pokedex"];
-    nom = poke["Nom"].toUpperCase();
-    types = [];
-    types.push(poke["Type1"].toLowerCase());
-    if ("Type2" in poke) {
-        types.push(poke["Type2"].toLowerCase());
-    }
-    for(i=0; i<nom.length; i++) {
-        $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
-    } 
-    $(".character").css("width", 90/nom.length + "%");
+    $.ajax({
+        url: "php/process.php",
+        type: "POST",
+        data: { SENDER: "NEWGAME", GENS: gens, REPLAY: 0 }
+    })
+    .done((data) => {
+        for(i=0; i<data["guess"].length; i++) {
+            $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
+        } 
+        $(".character").css("width", 90/data["guess"].length + "%");
+    });
 
     $("#replay").click(() => {
         $("#hint").removeClass("hint");
@@ -55,22 +35,20 @@ $(document).ready(function () {
                 gens.push(i);
             }
         }
-        poke = getRandomPokemon(gens);
-        pokedex = poke["Pokedex"];
-        nom = poke["Nom"].toUpperCase();
-        types = [];
-        types.push(poke["Type1"].toLowerCase());
-        if ("Type2" in poke) {
-            types.push(poke["Type2"].toLowerCase());
-        }
-        for(i=0; i<nom.length; i++) {
-            $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
-        } 
-        $(".character").css("width", 90/nom.length + "%");
+        $.ajax({
+            url: "php/process.php",
+            type: "POST",
+            data: { SENDER: "NEWGAME", GENS: gens, REPLAY: 1 }
+        })
+        .done((data) => {
+            for(i=0; i<data["guess"].length; i++) {
+                $("#row").append($("<div'></div>").addClass("character").append("<p></p>").text("_"));
+            } 
+            $(".character").css("width", 90/data["guess"].length + "%");
+        });
+
         $("#letters").text("Lettres :");
         $("#errors").text("Erreurs : 0/10");
-        found = 0;
-        errors = 0;
         for(i=65; i<91; i++) {
             $("#"+String.fromCharCode(i)).removeClass("used");
         }
@@ -94,12 +72,19 @@ $(document).ready(function () {
 
     $("#hint").click(() => {
         $("#hint").addClass("hint");
-        $("#type1").attr("src", "images/typesIcons/"+types[0]+".png");
-        $("#hints").show();
-        if (types.length > 1) {
-            $("#type2").attr("src", "images/typesIcons/"+types[1]+".png");
-            $("#type2").show();
-        }
+        $.ajax({
+            url: "php/process.php",
+            type: "POST",
+            data: { SENDER: "HINT" }
+        })
+        .done((data) => {
+            $("#type1").attr("src", "images/typesIcons/"+data["hint"][0]+".png");
+            $("#hints").show();
+            if (data["hint"].length > 1) {
+                $("#type2").attr("src", "images/typesIcons/"+data["hint"][1]+".png");
+                $("#type2").show();
+            }
+        });
     });
 
     $(".tile").click((event) => {
@@ -110,60 +95,39 @@ $(document).ready(function () {
             $("#Space").addClass("used");
             var id = event.target.id;
             let code = id.charCodeAt(0);
-            for(i=0; i<nom.length; i++) {
-                if (nom[i] == String.fromCharCode(code)) {
-                    $("#row").children()[i].innerHTML = "<p>"+ nom[i] +"</p>";
-                    found += 1;
+            $.ajax({
+                url: "php/process.php",
+                type: "POST",
+                data: { SENDER: "KEYDOWN", "KEYDOWN": String.fromCharCode(code) }
+            })
+            .done((data) => {
+                if (data["message"] == "Char not in name") {
+                    $("#errors").text("Erreurs : "+data["nbErrors"]+"/10");
+                } else {
+                    for(i=0; i<data["guess"].length; i++) {
+                        $("#row").children()[i].innerHTML = "<p>"+ data["guess"][i] +"</p>";
+                    }
                 }
-            }
-            let letters = $("#letters").text().split(":")[1];
-            if (letters.search(String.fromCharCode(code)) === -1) {
-                if (nom.search(String.fromCharCode(code)) === -1) {
-                    errors += 1;
-                    $("#errors").text("Erreurs : "+errors+"/10");
+    
+                if (data["found"]) {
+                    $(".character").css("background-color", "#0F0");
+                    $("#answer").attr("src", "https://www.pokebip.com/pokedex-images/300/"+data["pokedex"]+".png?v=ev-blueberry");
+                    end = true;
+                    $("#hint").addClass("hint");
+                    $("#score").text("Score : "+data["score"]);
                 }
-                $("#letters").text($("#letters").text()+" "+String.fromCharCode(code));
-            }
-            if (found >= nom.length) {
-                $(".character").css("background-color", "#0F0");
-                found = 0;
-                toAdd = 10-errors;
-                if ($("#hint").hasClass("hint")) {
-                    toAdd -= 2;
+    
+                if (data["nbErrors"] >= 10) {
+                    $(".character").css("background-color", "#F00");
+                    for(i=0; i<data["answer"].length; i++) {
+                        $("#row").children()[i].innerHTML = "<p>"+ data["answer"][i] +"</p>";
+                    }
+                    $("#hint").addClass("hint");
+                    $("#answer").attr("src", "https://www.pokebip.com/pokedex-images/300/"+data["pokedex"]+".png?v=ev-blueberry");
+                    end = true;
+                    $("#score").text("Score : 0");
                 }
-                if (toAdd < 0) {
-                    toAdd = 0;
-                }
-                score += toAdd;
-                errors = 0;
-                $("#score").text("Score : "+score);
-                let username = $("#session_user").text().split(' ')[1];
-                let tmp = Number(score).toString(2);
-                toSend = (tmp+"1").split("").reverse().join("");
-                toSend = parseInt(toSend, 2);
-                $.ajax({
-                    url: "php/updateScores.php",
-                    type: "POST",
-                    data: { name: username, score: toSend }
-                });
-                $("#answer").attr("src", "https://www.pokebip.com/pokedex-images/300/"+pokedex+".png?v=ev-blueberry");
-                $("#tiles").hide();
-                $("#answer").show();
-                $("#hint").addClass("hint");
-            }
-            if (errors >= 10) {
-                $(".character").css("background-color", "#F00");
-                for(i=0; i<nom.length; i++) {
-                    $("#row").children()[i].innerHTML = "<p>"+ nom[i] +"</p>";
-                }
-                score = 0;
-                errors = 0;
-                found = 0;
-                $("#answer").attr("src", "https://www.pokebip.com/pokedex-images/300/"+pokedex+".png?v=ev-blueberry");
-                $("#tiles").hide();
-                $("#answer").show();
-                $("#hint").addClass("hint");
-            }
+            });
             letters = $("#letters").text().split(":")[1];
             for(i=65; i<91; i++) {
                 if (letters.search(String.fromCharCode(i)) === -1) {
